4d9ad026511e87d750659245940f899bfdbfb2a1
carter-riley <carter.j.riley@outlook.com>
2016-12-05 00:04:15 -0800

    Made changes to begin testing mouse and arrow keys for movement and looking

--- RileyEricksonFinalProject/main.cpp
+++ RileyEricksonFinalProject/main.cpp
@@ -188,6 +188,16 @@ void drawScene(void)
    // Calculate and update modelview matrix.
    modelViewMat = mat4(1.0);
    modelViewMat = lookAt(vec3(0.0, 10.0, 15.0 + d), vec3(0.0, 10.0, 0.0), vec3(0.0, 1.0, 0.0));
+   /*(xVal - 10 * sin( (PI / 180.0) * angle),
+              0.0,
+              zVal - 10 * cos( (PI / 180.0) * angle),
+              xVal - 11 * sin( (PI / 180.0) * angle),
+              0.0,
+              zVal - 11 * cos( (PI / 180.0) * angle),
+              0.0,
+              1.0,
+              0.0);
+   */
    glUniformMatrix4fv(modelViewMatLoc, 1, GL_FALSE, value_ptr(modelViewMat));
 
    // Draw field.
@@ -234,7 +244,185 @@ void specialKeyInput(int key, int x, int y)
       if (d < 15.0) d += 0.1;
    }
    glutPostRedisplay();
+
+   /*
+   void specialKeyInput(int key, int x, int y)
+{
+    float tempxVal = xVal, tempzVal = zVal, tempAngle = angle;
+
+    // Compute next position.
+    if (key == GLUT_KEY_LEFT) tempAngle = angle + 5.0;
+    if (key == GLUT_KEY_RIGHT) tempAngle = angle - 5.0;
+    if( key == GLUT_KEY_UP)
+    {
+        tempxVal = xVal - sin(angle * PI / 180.0);
+        tempzVal = zVal - cos(angle * PI / 180.0);
+    }
+    if( key == GLUT_KEY_DOWN)
+    {
+        tempxVal = xVal + sin(angle * PI / 180.0);
+        tempzVal = zVal + cos(angle * PI / 180.0);
+    }
+
+    // Angle correction.
+    if (tempAngle > 360.0) tempAngle -= 360.0;
+    if (tempAngle < 0.0) tempAngle += 360.0;
+
+    // Move spacecraft to next position only if there will not be collision with an asteroid.
+    if (!asteroidCraftCollision(tempxVal, tempzVal, tempAngle, false) )
+    {
+        isCollision = 0;
+        xVal = tempxVal;
+        zVal = tempzVal;
+        angle = tempAngle;
+    }
+    else isCollision = true;
+
+    glutPostRedisplay();
+    */
 }
+}
+
+/*
+void
+mouse( GLint button, GLint state, GLint x, GLint y )
+{
+    static GLint buttons_down = 0;
+
+    if (state == GLUT_DOWN)
+    {
+        // cout << "mouse: button = " << button << "  state = " << state << "  x,y = " << x << "," << y << "\n";
+
+        switch (button)
+        {
+        case GLUT_LEFT_BUTTON:
+            // cout << "     mouse: GLUT_LEFT_BUTTON - TUMBLE\n";
+            action = TUMBLE;
+            xStart = x;
+            yStart = y;
+            break;
+        case GLUT_MIDDLE_BUTTON:
+            //  cout << "     mouse: GLUT_MIDDLE_BUTTON - DOLLY\n";
+            action = DOLLY;
+            xStart = x;
+            yStart = y;
+            break;
+        case GLUT_RIGHT_BUTTON:
+            //  cout << "     mouse: GLUT_RIGHT_BUTTON - TRACK\n";
+            action = TRACK;
+            xStart = x;
+            yStart = y;
+            break;
+        }
+    }
+    if (state == GLUT_UP)
+    {
+        // printControls();
+    }
+}
+
+//---------------------------------------------------------------------------- tumble
+// tumble about to location tumblePoint in World Coordinate System.
+void tumble(mat4 rx, mat4 ry, vec4 tumblePoint)
+{
+    // We want to rotate about the world coordinate system along a direction parallel to the
+    // camera's x axis. We first determine the coordinates of the WCS origin expressed in the eye coordinates.
+    // We then translate this point to the camera (origin in camera coordinates) and do a rotation about x.
+    // We then translate back. The result is then composed with the view matrix to give a new view matrix.
+   //  NEED TO IMPLEMENT !!!   When done, should have new value for eye, u, v, n
+
+   // DO THIS CONTROL LAST - IT IS THE MOST DIFFICULT PART
+
+   mat4 view  =  viewRotation * Translate( -eye );  // current view matrix
+
+   // X Rotate about tumble point in Camera Coord Sys
+   vec4 loc = view * tumblePoint;  // location of tumble point in cam coord
+   mat4 t2 = Translate(loc[0],loc[1],loc[2]);
+   mat4 t1 = Translate(-loc[0],-loc[1],-loc[2]);
+   mat4 xtransform = t2 * rx * t1;
+
+   // Y Rotate about tumble point in WCS
+    t2 = Translate(tumblePoint[0],tumblePoint[1],tumblePoint[2]);
+    t1 = Translate(-tumblePoint[0],-tumblePoint[1],-tumblePoint[2]);
+    mat4 ytransform = t2 * ry * t1;
+
+    mat4 newView = xtransform * view * ytransform;
+
+    // need to get eye position back
+    //  Here, rotInverse is the inverse of the rotational part of the view matrix.
+    //  eye = -rotInverse*view*origin  -> this gives the location of the WCS origin in the eye coordinates
+    mat4 rotInverse = transpose(newView);
+    rotInverse[3][0] = 0;
+    rotInverse[3][1] = 0;
+    rotInverse[3][2] = 0; // zero out translational part
+    rotInverse[3][3] = 1;
+    eye = -(rotInverse * newView * vec4(0,0,0,1));
+
+    viewRotation = newView;
+    viewRotation[0][3] = 0;
+    viewRotation[1][3] = 0;
+    viewRotation[2][3] = 0;
+    viewRotation[3][3] = 1;
+}
+
+//---------------------------------------------------------------------------- motion
+void
+motion( GLint x, GLint y )
+{
+    float dx, dy;
+    mat4 ry, rx;
+    vec4 tumblePoint;
+
+    switch (action)
+    {
+    case TUMBLE:
+        dx = 0.05*(x - xStart);
+        dy = 0.05*(y - yStart);
+
+        ry = RotateY(10 * dx);
+        rx = RotateX(10 * dy);
+
+        // tumble about a point tumblePoint in WCS. Two options currently.
+        if (t == 0)   // tumble about origin in  WCS
+        {
+            tumblePoint =  vec4(0,0,0,1);
+        }
+        else        // tumble about fixed distance in front of camera in  WCS
+        {
+            tumblePoint =   eye - d*viewRotation[2];
+            tumblePoint[3] = 1;
+        }
+        tumble(rx, ry, tumblePoint);   //  <----  NEED TO IMPLEMENT this method!!!
+
+        xStart = x;
+        yStart = y;
+        break;
+    case TRACK:  // PAN
+        dx = -0.05 *
+        (x - xStart);
+        dy = 0.05*(y - yStart);
+         //  NEED TO IMPLEMENT !!!
+        eye = eye + dx*viewRotation[0] + dy*viewRotation[1];
+        eye[3]=1;
+        xStart = x;
+        yStart = y;
+        break;
+    case DOLLY:
+        dy = 0.05*(y - yStart);
+        dx = 0.05*(x - xStart);
+         //  NEED TO IMPLEMENT !!!
+         eye = eye - (dy+dx)*viewRotation[2];
+        eye[3]=1;
+        xStart = x;
+        yStart = y;
+        break;
+    default:
+        printf("unknown action %d\n", action);
+    }
+
+    glutPostRedisplay();
+}
+*/
 
 // Routine to output interaction instructions to the C++ window.
 void printInteraction(void)
