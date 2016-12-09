6a6d70f2d24ca201c0b97d67dac41dd421870b34
carter-riley <carter.j.riley@outlook.com>
2016-12-09 01:06:21 -0800

    Sparkles

--- RileyEricksonFinalProject/fragmentShader.glsl
+++ RileyEricksonFinalProject/fragmentShader.glsl
@@ -1,7 +1,6 @@
 #version 430 core
 
 in vec2 texCoordsExport;
-in vec4 colsExport;
 
 uniform sampler2D grassTex;
 uniform sampler2D sky1Tex;
@@ -17,19 +16,17 @@ vec4 fieldTexColor, sky1TexColor, sky2TexColor, sky3TexColor, sky4TexColor, clou
 
 void main(void)
 {
-    fieldTexColor = texture(grassTex, texCoordsExport);
-    sky1TexColor = texture(sky1Tex, texCoordsExport);
-    sky2TexColor = texture(sky2Tex, texCoordsExport);
-    sky3TexColor = texture(sky3Tex, texCoordsExport);
-    sky4TexColor = texture(sky4Tex, texCoordsExport);
-    cloudTexColor = texture(cloudTex, texCoordsExport);
+   fieldTexColor = texture(grassTex, texCoordsExport);
+   sky1TexColor = texture(sky1Tex, texCoordsExport);
+   sky2TexColor = texture(sky2Tex, texCoordsExport);
+   sky3TexColor = texture(sky3Tex, texCoordsExport);
+   sky4TexColor = texture(sky4Tex, texCoordsExport);
+   cloudTexColor = texture(cloudTex, texCoordsExport);
 
-    if (object == 0) colorsOut = fieldTexColor;
-    if (object == 1) colorsOut = sky1TexColor;
-    if (object == 2) colorsOut = sky2TexColor;
-    if (object == 3) colorsOut = sky3TexColor;
-    if (object == 4) colorsOut = sky4TexColor;
-    if (object == 5) colorsOut = cloudTexColor;
-    if (object == 6) colorsOut = colsExport;
-
+   if (object == 0) colorsOut = fieldTexColor;
+   if (object == 1) colorsOut = sky1TexColor;
+   if (object == 2) colorsOut = sky2TexColor;
+   if (object == 3) colorsOut = sky3TexColor;
+   if (object == 4) colorsOut = sky4TexColor;
+   if (object == 5) colorsOut = cloudTexColor;
 }
